ced2a5ab9c941c4ef7ffcac65606c7993148a876
Weslei Batista <wesbatistaa@gmail.com>
2025-04-09 11:28:21 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='aplicacao aprendizado mlops',
+    author='Batista94',
+    license='',
+)
